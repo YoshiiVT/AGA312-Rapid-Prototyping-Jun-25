e09cfe89df0d68cf9f8f830688c997f5a5fd2b57
YoshiiVT <YoshiiUkichiro@outlook.com>
2025-07-15 16:00:11 +1000

    Got Objects falling from the sky onto the platform

--- Assets/Prototype_III/Scripts/ChuteManager.cs
+++ Assets/Prototype_III/Scripts/ChuteManager.cs
@@ -1,18 +1,33 @@
 using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
 namespace PROTOTYPE_3
 {
     public class ChuteManager : GameBehaviour<ChuteManager>
     {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
-        {
+        [SerializeField] private GameObject chuteDispenser;
 
+        [SerializeField] private List<ChuteObjData> chuteObjList;
+
+        [SerializeField] private GameObject chuteObjPrefab;
+
+        private int objectCounter;
+        
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.W)) SpawnObj();
         }
 
-        // Update is called once per frame
-        void Update()
+        private void SpawnObj()
         {
-
+            if(objectCounter <= chuteObjList.Count) 
+            {
+                GameObject chuteObj = Instantiate(chuteObjPrefab, chuteDispenser.transform);
+                chuteObj.GetComponent<ChuteOBJ>().Initialize(chuteObjList[objectCounter]);
+            }
+            
         }
     }
 }
---
+++ Assets/Prototype_III/Scripts/ChuteOBJ.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class ChuteOBJ : MonoBehaviour
+{
+    [SerializeField] private ChuteObjData chuteObjData;
+    [SerializeField] private Color renderColour;
+    [SerializeField] private ChuteObject objectType;
+    public void Initialize(ChuteObjData _chuteObjData)
+    {
+        chuteObjData = _chuteObjData;
+        renderColour = chuteObjData.meshColour;
+        gameObject.GetComponent<Renderer>().material.color = renderColour;
+        objectType = chuteObjData.chuteObj;
+    }
+}
