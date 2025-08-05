ac71752786c10522720bf9dd6e45707bf9d1ffbc
YoshiiVT <YoshiiUkichiro@outlook.com>
2025-08-05 14:47:55 +1000

    Added lifespan to obstacles, obstacles now self destroy

--- Assets/Prototype_IV/Scripts/Obstacle.cs
+++ Assets/Prototype_IV/Scripts/Obstacle.cs
@@ -1,11 +1,24 @@
+using System.Collections;
 using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
-    [SerializeField] private float speed; 
+    [SerializeField] private float speed;
+    [SerializeField] private float lifespan = 5;
+
+    private void Start()
+    {
+        StartCoroutine(LifeSpan());
+    }
 
     void Update()
     {
         transform.position += Vector3.left * speed * Time.deltaTime;
+    }
+
+    private IEnumerator LifeSpan()
+    {
+        yield return new WaitForSeconds(lifespan);
+        Destroy(gameObject);
     }
 }
